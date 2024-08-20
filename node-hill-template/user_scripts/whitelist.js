8d9b9193339a412bfa4c6eaa7b884fde38e6c6ca
Yekc <71790420+Yekc@users.noreply.github.com>
2024-08-19 19:49:53 -0500

    Update whitelist.js

--- node-hill-template/user_scripts/whitelist.js
+++ node-hill-template/user_scripts/whitelist.js
@@ -1,5 +1,6 @@
 Game.authHandler = async(player) => {
     if (player.username !== "Yek") {
+        console.log(player.username)
         return player.kick("this game is whitelisted for now")
     }
     return true
