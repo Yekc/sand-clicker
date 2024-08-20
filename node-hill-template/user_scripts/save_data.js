8e4801945b42e4dc41f07f57a2d06cca7f6b0e22
Yekc <71790420+Yekc@users.noreply.github.com>
2024-08-20 14:48:57 -0500

    yea

--- node-hill-template/user_scripts/save_data.js
+++ node-hill-template/user_scripts/save_data.js
@@ -1,7 +1,8 @@
-const { QuickDB } = getModule("quick.db")
-db = new QuickDB()
+const fs = getModule("fs")
 
-/*const saveInterval = 600000
+const saveInterval = 600000
+
+if (fs.existsSync(`./player_data/`) === false) fs.mkdirSync("./player_data/")
 
 baseStats = {
     sand: 0
@@ -20,13 +21,19 @@ const updates = {
 }
 
 save = async function(player) {
+    /*
     db.set(player.userId.toString(), player.data)
     console.log(`DEBUG >>> Saved user data for ${player.username} (${player.userId})!`)
+    */
+    let data = JSON.stringify(player.data)
+    fs.writeFileSync(`./player_data/${player.userId}.txt`, data)
+    console.log(`Saved user data for ${player.username} (${player.userId})!`)
 }
 
 load = async function(player) {
     player.sand = baseStats.sand
 
+    /*
     if (await db.has(player.userId.toString())) {
         player.data = await db.get(player.userId.toString())
         console.log(`DEBUG >>> Loaded user data for ${player.username} (${player.userId})!`)
@@ -42,7 +49,25 @@ load = async function(player) {
         player.data = await db.get(player.userId.toString())
         console.log(`DEBUG >>> Created user data for ${player.username} (${player.userId})!`)
     }
+    */
 
+    try {
+        if (fs.existsSync(`./player_data/${player.userId}.txt`)) {
+            player.data = JSON.parse(fs.readFileSync(`./player_data/${player.userId}.txt`).toString())
+            console.log(`Loaded user data for ${player.username} (${player.userId})!`)
+
+            if (player.data.saveDataVersion < currentTemplateVersion) {
+                let pv = player.data.saveDataVersion
+                let f = updates["from" + String(currentTemplateVersion)]
+                f(player)
+                console.log(`Updated user data for ${player.username} (${player.userId}) from ${pv} to ${currentTemplateVersion}!`)
+            }
+        }
+    } catch (error) {
+        console.error(error)
+        player.kick(error.name)
+    }
+    
     player.emit("Loaded")
 }
 
@@ -66,4 +91,3 @@ Game.bindToClose(() => {
         save(player)
     })
 })
-*/
