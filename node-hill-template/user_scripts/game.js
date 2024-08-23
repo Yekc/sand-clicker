3d0b6fcd50efaf02358c1b7e8e369e4e43a6982d
Yekc <71790420+Yekc@users.noreply.github.com>
2024-08-22 20:01:19 -0500

    Update game.js

--- node-hill-template/user_scripts/game.js
+++ node-hill-template/user_scripts/game.js
@@ -4,14 +4,13 @@ Game.MOTD = ""
 
 //Restart after 2 hours
 setTimeout(() => {
-    console.log("test1")
-    Game.messageAll.message("\\c6!!! Server restarting in 1 minute !!!")
+    Game.messageAll("\\c6!!! Server restarting in 1 minute !!!")
 
     Game.players.forEach(player => {
         save(player)
     })
 
-    setTimeout(() => {Game.messageAll.message("\\c6!!! Server restarting in 10 seconds !!!")}, 10000)
+    setTimeout(() => {Game.messageAll("\\c6!!! Server restarting in 10 seconds !!!")}, 10000)
     setTimeout(() => {Game.shutdown()}, 60000)
 //}, 7200000)
 }, 70000)
