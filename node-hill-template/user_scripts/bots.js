3b8b605251ab87dec7b5dc4728507f11a0f1bd8d
Yekc <71790420+Yekc@users.noreply.github.com>
2024-08-23 01:09:16 -0500

    finale

--- node-hill-template/user_scripts/bots.js
+++ node-hill-template/user_scripts/bots.js
@@ -247,10 +247,10 @@ Game.on("playerJoin", (player) => {
                 
                 //Pet inventory scrolling
                 case "z":
-                    if (player.pet_inv && player.pet_inv_page > 1) player.pet_inv_page--
+                    if (player.pet_inv && player.pet_inv_view == 0 && player.pet_inv_page > 1) player.pet_inv_page--
                     break;
                 case "x":
-                    if (player.pet_inv && player.pet_inv_page < global.max_pet_inv_page) player.pet_inv_page++
+                    if (player.pet_inv && player.pet_inv_view == 0  && player.pet_inv_page < global.max_pet_inv_page) player.pet_inv_page++
                     break;
                 
                 //Equip pet
--- node-hill-template/user_scripts/pets.js
+++ node-hill-template/user_scripts/pets.js
@@ -82,9 +82,7 @@ Game.on("playerJoin", (player) => {
 
                     draw += `##\\c0Page ${player.pet_inv_page == 1 ? "\\c1" : ""}< \\c0${player.pet_inv_page}/${global.max_pet_inv_page} ${player.pet_inv_page == global.max_pet_inv_page ? "\\c1" : ""}>    \\c1Use \\c7Z \\c1and \\c7X \\c1to scroll through the pages`
                 } else {
-                    console.log((player.pet_inv_view - 1) + (9 * (player.pet_inv_page - 1)))
                     let current_pet = getPet(Object.keys(player.data.pets)[(player.pet_inv_view - 1) + (9 * (player.pet_inv_page - 1))])
-                    console.log(current_pet)
 
                     if (player.data.pet_active === current_pet.id) draw += "#\\c5You have this pet equipped!#"
 
