2da7de746d0ff7ef99ce3d324db774e5d2463d49
Yekc <71790420+Yekc@users.noreply.github.com>
2024-08-23 00:07:43 -0500

    Update sand.js

--- node-hill-template/user_scripts/sand.js
+++ node-hill-template/user_scripts/sand.js
@@ -132,9 +132,9 @@ click_brick.clicked(debouncePlayer((player, secure) => {
 
         //Give the pet to the player
         if (choose.length > 0) {
-            console.log(`\\c5You found a${(rarity == 1 || rarity == 4) ? "n" : ""} ${getRarityColor(rarity)}${getRarityName(rarity)} ${pet.display.name}\\c5!`)
-            player.message(`\\c5You found a${(rarity == 1 || rarity == 4) ? "n" : ""} ${getRarityColor(rarity)}${getRarityName(rarity)} ${pet.display.name}\\c5!`)
+            console.log(`${pet.display.name}`)
             earnPet(player, choose[Math.floor(Math.random() * choose.length)].id)
+            player.message(`\\c5You found a${(rarity == 1 || rarity == 4) ? "n" : ""} ${getRarityColor(rarity)}${getRarityName(rarity)} ${pet.display.name}\\c5!`)
             player.centerPrint(`\\c5You found a${(rarity == 1 || rarity == 4) ? "n" : ""} ${getRarityColor(rarity)}${getRarityName(rarity)} ${pet.display.name}\\c5!`, 5)
             player.should_say = false
         }
