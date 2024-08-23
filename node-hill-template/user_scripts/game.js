ce12fdfe9d11263bb02f8764ce8cba1a42da97f0
Yekc <71790420+Yekc@users.noreply.github.com>
2024-08-22 19:56:18 -0500

    Update game.js

--- node-hill-template/user_scripts/game.js
+++ node-hill-template/user_scripts/game.js
@@ -2,6 +2,19 @@
 console.error("----------------------------------------")
 Game.MOTD = ""
 
+//Restart after 2 hours
+setTimeout(() => {
+    Game.messageAll.message("\\c6!!! Server restarting in 1 minute !!!")
+
+    Game.players.forEach(player => {
+        save(player)
+    })
+
+    setTimeout(() => {Game.messageAll.message("\\c6!!! Server restarting in 10 seconds !!!")}, 10000)
+    setTimeout(() => {Game.shutdown()}, 60000)
+//}, 7200000)
+}, 120000)
+
 //Random bricks
 Game.world.bricks.forEach(brick => {
     if (brick.name === "random" || brick.name === "random_super") brick.setVisibility(0)
