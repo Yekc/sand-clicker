b2fa5a7dde822eeae56ca775f0ab124c664932cc
Yekc <71790420+Yekc@users.noreply.github.com>
2024-08-22 23:35:36 -0500

    Update sand.js

--- node-hill-template/user_scripts/sand.js
+++ node-hill-template/user_scripts/sand.js
@@ -100,8 +100,7 @@ click_brick.clicked(debouncePlayer((player, secure) => {
     }
 
     //Chance to earn a pet
-    //let pet_roll = Math.round(Math.random() * 40000)
-    let pet_roll = Math.round(Math.random() * 100)
+    let pet_roll = Math.round(Math.random() * 40000)
     let rarity = 0
     if (pet_roll < 2) {
         rarity = 6
