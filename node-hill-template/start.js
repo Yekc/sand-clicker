fe021be2e1c19fc965a5bb2e4d1d296354051133
Yekc <71790420+Yekc@users.noreply.github.com>
2024-08-25 13:20:21 -0500

    1.3 ready

--- node-hill-template/start.js
+++ node-hill-template/start.js
@@ -1,11 +1,11 @@
 const nh = require('node-hill-s')
 
 nh.startServer({
-    //hostKey: "ZbqFHImC73riat2FphDPwKp0CwJlbyepiQoKY8G3S64fSiZT6idG5S6TTyQTDJJC", //MAIN
-    hostKey: "Whuapq6U2i6YwIiNk5UEUlLdkozWgZAkSiSQUUuvbrZVZH4w34QI3PWlhhMUHQtE", //TESTING
+    hostKey: "ZbqFHImC73riat2FphDPwKp0CwJlbyepiQoKY8G3S64fSiZT6idG5S6TTyQTDJJC", //MAIN
+    //hostKey: "Whuapq6U2i6YwIiNk5UEUlLdkozWgZAkSiSQUUuvbrZVZH4w34QI3PWlhhMUHQtE", //TESTING
 
-    //gameId: 9, //MAIN
-    gameId: 10, //TESTING
+    gameId: 9, //MAIN
+    //gameId: 10, //TESTING
 
     port: 42481,
 
